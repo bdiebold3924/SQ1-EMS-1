537a547c000d6cadfadd1c00659b6c5dff062e82
jason schneider <jason schneider>
2015-12-07 11:47:13 -0500

    Signed-off-by: Admin <Admin> Signed-off-by: jason schneider <jason schneider>

--- LoggerSQ/LoggerSQ/Program.cs
+++ LoggerSQ/LoggerSQ/Program.cs
@@ -11,8 +11,18 @@ namespace LoggerSQ
     {
         static void Main(string[] args)
         {
+            String date = Logger.parseDate();
+            int dateInt = Logger.dateToInt(date);
+            Console.WriteLine(dateInt.ToString());
             //using System.Reflection
             Console.WriteLine(Logger.TestLog(MethodBase.GetCurrentMethod().DeclaringType.Name, MethodBase.GetCurrentMethod().Name, "Meh"));
+            Logger.Log(MethodBase.GetCurrentMethod().DeclaringType.Name, MethodBase.GetCurrentMethod().Name, "even more in the future! ");
+            Logger.Log(MethodBase.GetCurrentMethod().DeclaringType.Name, MethodBase.GetCurrentMethod().Name, "2nd Log");
+            Logger.Log(MethodBase.GetCurrentMethod().DeclaringType.Name, MethodBase.GetCurrentMethod().Name, "3rd");
+            Logger.Log(MethodBase.GetCurrentMethod().DeclaringType.Name, MethodBase.GetCurrentMethod().Name, "MORE logging");
+            Logger.Log(MethodBase.GetCurrentMethod().DeclaringType.Name, MethodBase.GetCurrentMethod().Name, "asfsdfasdfdsf");
+            Logger.Log(MethodBase.GetCurrentMethod().DeclaringType.Name, MethodBase.GetCurrentMethod().Name, "Employee 2 fired");
+
         }
     }
 }
